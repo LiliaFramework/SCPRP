0dd6df5accdaefa04f83995f3525af26e9b8e3a5
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 17:47:23 -0400

    Doc 049 Class

--- scprp/schema/classes/scp_049.lua
+++ scprp/schema/classes/scp_049.lua
@@ -1,3 +1,21 @@
+--- Attributes of the SCP-049 class.
+-- @class SCP-049
+
+--- A list of the current fields in the class.
+-- @realm shared
+-- @table Configurations
+-- @field name The name of the class | **string**
+-- @field desc The description of the class | **string**
+-- @field isDefault Whether the class is set as default | **boolean**
+-- @field model The model of the class | **string**
+-- @field health The health of the class | **number**
+-- @field walkSpeed The walking speed of the class | **number**
+-- @field runSpeed The running speed of the class | **number**
+-- @field bloodcolor The blood color of the class | **number**
+-- @field faction The faction of the class | **number**
+-- @field OnSet A function that is called when the class is set | **function**
+-- @field index The Unique ID of the class | **number**
+
 --------------------------------------------------------------------------------------------------------
 CLASS.name = "SCP-049"
 CLASS.desc = "A humanoid figure that resembles a 15-16th century European Plague Doctor, with a black robe and a white-beaked mask stands before you."
