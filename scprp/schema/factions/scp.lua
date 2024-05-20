7f737a7d116a9bf1ad2b842a813a54e95e6fc32a
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 17:42:24 -0400

    Doc scp.lua

--- scprp/schema/factions/scp.lua
+++ scprp/schema/factions/scp.lua
@@ -1,16 +1,26 @@
---------------------------------------------------------------------------------------------------------
+--- Attributes of the default faction.
+-- @faction SCP
+
+--- A list of the current fields in the faction.
+-- @realm shared
+-- @table Configurations
+-- @field name The name of the faction | **string**
+-- @field desc The description of the faction | **string**
+-- @field color The color representing the faction | **Color**
+-- @field isDefault Whether the faction is set as default | **boolean**
+-- @field index The Unique ID of the faction | **number**
+
 FACTION.name = "SCP's"
 FACTION.desc = "Select your SCP."
 FACTION.color = Color(68,68,68)
 FACTION.isDefault = true
---------------------------------------------------------------------------------------------------------
+
 function FACTION:GetDefaultName(_)
 	return "SCP" .. math.radom(11111, 99999)
 end
---------------------------------------------------------------------------------------------------------
+
 function FACTION:GetDefaultDesc(_, _)
 	return "A Character that is used to play as an SCP."
 end
---------------------------------------------------------------------------------------------------------
+
 FACTION_SCP = FACTION.index
---------------------------------------------------------------------------------------------------------
