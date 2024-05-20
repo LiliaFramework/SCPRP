e1349c94fec5ef9478120698f8d11e3232188223
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 17:00:35 -0400

    Add global table > SCHEMA.SCPs

--- scprp/schema/schema.lua
+++ scprp/schema/schema.lua
@@ -16,13 +16,7 @@ SCHEMA.name = "SCPRP"
 SCHEMA.author = "Sample Schema Author"
 SCHEMA.desc = "A SCPRP Schema"
 
-lia.chat.register("mind", {
-	onChatAdd = function(_, text)
-		chat.AddText(Color(115, 115, 115), "**\""..text.."\"")
-	end,
-	onCanHear = 1,
-	prefix = {"/mind"},
-	font = "liaChatFontItalics",
-	filter = "actions",
-	deadCanChat = true
-})
+--- A list containing the schema's SCPs.
+SCHEMA.SCPs = {
+	CLASS_SCP_049
+}
