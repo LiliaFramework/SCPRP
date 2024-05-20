64680aba8cdc9ea8748032aad43bb13b866542f6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-20 22:56:02 +0100

    Removed Separators

--- scprp/schema/attributes/pestilence.lua
+++ scprp/schema/attributes/pestilence.lua
@@ -1,6 +1,5 @@
-------------------------------------------------------------------------------------
+
 ATTRIBUTE.name = "Pestilence"
 ATTRIBUTE.desc = "You are a carrier of the pestilence."
 ATTRIBUTE.noStartBonus = true
 ATTRIBUTE.NoF1Show = true
-------------------------------------------------------------------------------------
--- scprp/schema/classes/scp_049.lua
+++ scprp/schema/classes/scp_049.lua
@@ -16,7 +16,7 @@
 -- @field OnSet A function that is called when the class is set | **function**
 -- @field index The Unique ID of the class | **number**
 
---------------------------------------------------------------------------------------------------------
+
 CLASS.name = "SCP-049"
 CLASS.desc = "A humanoid figure that resembles a 15-16th century European Plague Doctor, with a black robe and a white-beaked mask stands before you."
 CLASS.isDefault = true
@@ -26,7 +26,7 @@ CLASS.walkSpeed = 150
 CLASS.runSpeed = 150
 CLASS.bloodcolor = DONT_BLEED
 CLASS.faction = FACTION_SCP
---------------------------------------------------------------------------------------------------------
+
 function CLASS:OnSet(client)
 	client:Spawn()
 	local char = client:getChar()
@@ -34,6 +34,5 @@ function CLASS:OnSet(client)
 	char:setDesc(self.desc)
 	lia.chat.send(client, "mind", "You sense the presence of the peastilence within this realm.")
 end
---------------------------------------------------------------------------------------------------------
+
 CLASS_SCP_049 = CLASS.index
---------------------------------------------------------------------------------------------------------
