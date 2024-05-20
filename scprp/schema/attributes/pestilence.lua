59c92a927a3421b611a91a98801f3027a0941b1f
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 17:24:58 -0400

    Lint

--- scprp/schema/attributes/pestilence.lua
+++ scprp/schema/attributes/pestilence.lua
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------------------
 ATTRIBUTE.name = "Pestilence"
 ATTRIBUTE.desc = "You are a carrier of the pestilence."
-ATTRIBUTE.noStartBonus = true 
+ATTRIBUTE.noStartBonus = true
 ATTRIBUTE.NoF1Show = true
 ------------------------------------------------------------------------------------
