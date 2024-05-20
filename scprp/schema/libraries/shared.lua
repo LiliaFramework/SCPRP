ace65b83ed1eb9724ba01543b8c67333d3ff407e
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 17:00:20 -0400

    Create libraries\shared.lua add chat.register

---
+++ scprp/schema/libraries/shared.lua
@@ -0,0 +1,12 @@
+--------------------------------------------------------------------------------------------------------
+lia.chat.register("mind", {
+	onChatAdd = function(_, text)
+		chat.AddText(Color(115, 115, 115), "**\""..text.."\"")
+	end,
+	onCanHear = 1,
+	prefix = {"/mind"},
+	font = "liaChatFontItalics",
+	filter = "actions",
+	deadCanChat = true
+})
+--------------------------------------------------------------------------------------------------------
