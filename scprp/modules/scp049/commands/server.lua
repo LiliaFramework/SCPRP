28e40ad4124f910a03b319386b8571e14e89b22c
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 17:34:22 -0400

    Add 049 Module

---
+++ scprp/modules/scp049/commands/server.lua
@@ -0,0 +1,29 @@
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.command.add(
+	"sensepestilence",
+	{
+		onRun = function(client, arguments) 
+			if not client:Team() == CLASS_SCP_049 then
+				client:notify("You are not SCP-049.")
+				return
+			end
+
+			local target = client:GetEyeTrace().Entity
+
+			if not target:IsPlayer() then return end
+
+			client:setAction("You begin to examine the being before you...",
+			10,
+			function()
+				local num = target:getAttrib("pestilence")
+
+				if num >= 50 then
+					lia.chat.send(client, "mind", "You sense a large amount of the peastilence within this being.")
+				else
+					lia.chat.send(client, "mind", "You sense no peastilence within this being")
+				end
+			end)
+		end
+	}
+)
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
