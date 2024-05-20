70bb071bc89b9fbfe8d383a63b9c2ae17baea865
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 16:32:57 -0400

    Start on 049

---
+++ scprp/schema/classes/scp_049.lua
@@ -0,0 +1,19 @@
+--------------------------------------------------------------------------------------------------------
+CLASS.name = "SCP-049"
+CLASS.desc = "A humanoid figure that resembles a 15-16th century European Plague Doctor, with a black robe and a white-beaked mask stands before you."
+CLASS.isDefault = true
+CLASS.model = ""
+CLASS.health = 1000
+CLASS.bloodcolor = DONT_BLEED
+CLASS.faction = FACTION_SCP
+--------------------------------------------------------------------------------------------------------
+function CLASS:OnSet(client)
+	client:Spawn()
+	local char = client:getChar()
+	char:setName("SCP-049, 'The Plague Doctor'")
+	char:setDesc(self.desc)
+	client:notify("You have been set as SCP-049.")
+end
+--------------------------------------------------------------------------------------------------------
+CLASS_SCP_049 = CLASS.index
+--------------------------------------------------------------------------------------------------------
