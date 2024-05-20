56d641863506a54da0669aeef65c8d0baadbc319
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 18:32:54 -0400

    Basic Clearance Meta

---
+++ scprp/schema/meta/shared.lua
@@ -0,0 +1,18 @@
+local PLAYER = FindMetaTable("Player")
+local CHAR = lia.meta.character
+
+function CHAR:GetClearance()
+	return self:GetData("clearance", 0)
+end
+
+function CHAR:HasClearance(level)
+	return self:GetClearance() >= level
+end
+
+function PLAYER:HasClearance(level)
+	local char = self:getChar()
+	
+	if char then return char:GetClearance() >= level end
+
+	return false
+end
