7e0bc2416eaf2906eeae6613a96d667ecf6cf3c5
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 18:39:04 -0400

    Create Foundation Recruit Faction

---
+++ scprp/schema/factions/frecruit.lua
@@ -0,0 +1,26 @@
+--- Attributes of the Foundation Recruit faction.
+-- @factions Foundation Recruit
+
+--- A list of the current fields in the faction.
+-- @realm shared
+-- @table Configurations
+-- @field name The name of the faction | **string**
+-- @field desc The description of the faction | **string**
+-- @field color The color representing the faction | **Color**
+-- @field isDefault Whether the faction is set as default | **boolean**
+-- @field index The Unique ID of the faction | **number**
+
+FACTION.name = "Foundation Recruits"
+FACTION.desc = "New recruits to the SCP Foundation. They are awaiting training and are not yet assigned to a specific department."
+FACTION.color = Color(65,65,65)
+FACTION.isDefault = true
+-- Workshop ID for pack "2973533817"
+FACTION.models = {
+	"models/nostras/visitor/male_02_visitor.mdl",
+	"models/nostras/visitor/male_04_visitor.mdl",
+	"models/nostras/visitor/male_07_visitor.mdl",
+	"models/nostras/visitor/male_08_visitor.mdl",
+	"models/nostras/visitor/male_09_visitor.mdl"
+}
+
+FACTION_FRECRUIT = FACTION.index
