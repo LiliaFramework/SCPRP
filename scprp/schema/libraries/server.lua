528cb217bb55388d5e2200272ae7ad9d8f8b7938
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-20 18:28:19 -0400

    Update server.lua

--- scprp/schema/libraries/server.lua
+++ scprp/schema/libraries/server.lua
@@ -1,14 +1,7 @@
---- Server-side library functions.
--- reside within 'lia'
-
-
 function SCHEMA:PlayerSwitchFlashlight(_)
 	return true
 end
 
--- This function is used to get all the classes that belong to the SCP faction.
--- @return table A table of all the SCP classes.
--- @realm server
 function lia.scp.getSCPClasses()
 	local classes = {}
 	for _, v in pairs(lia.class.classes) do
@@ -18,3 +11,4 @@ function lia.scp.getSCPClasses()
 	end
 	return classes
 end
+
