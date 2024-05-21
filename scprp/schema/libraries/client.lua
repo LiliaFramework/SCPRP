dd1d326e9a79a59f2620f905fe67c09ff401c9e7
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-21 13:43:19 -0400

    682 Screen Effect

---
+++ scprp/schema/libraries/client.lua
@@ -0,0 +1,17 @@
+local color_682 = {}
+
+color_682[ "$pp_colour_addr" ] = 0,
+color_682[ "$pp_colour_addg" ] = 0,
+color_682[ "$pp_colour_addb" ] = 0,
+color_682[ "$pp_colour_brightness" ] = -0.7 or -0.3,
+color_682[ "$pp_colour_contrast" ] = 1,
+color_682[ "$pp_colour_colour" ] = 1,
+color_682[ "$pp_colour_mulr" ] = 5,
+color_682[ "$pp_colour_mulg" ] = 0,
+color_682[ "$pp_colour_mulb" ] = 0
+function SCHEMA:RenderScreenspaceEffects()
+	local client = LocalPlayer()
+	if client:getClass() == CLASS_SCP_682 then
+		DrawColorModify(color_682)
+	end
+end
