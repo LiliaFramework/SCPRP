df73e9d311a3e244834f102b2e7157fe3d4ee188
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-21 13:35:01 -0400

    Create scp_682.lua

---
+++ scprp/schema/classes/scp_682.lua
@@ -0,0 +1,19 @@
+CLASS.name = "SCP-682"
+CLASS.desc = "A large, vaguely reptile-like creature of unknown origin. It appears to be extremely intelligent."
+CLASS.isDefault = true
+CLASS.model = ""
+CLASS.health = 10000
+CLASS.walkSpeed = 200
+CLASS.runSpeed = 300
+CLASS.bloodcolor = DONT_BLEED
+CLASS.faction = FACTION_SCP
+
+function CLASS:OnSet(client)
+	client:Spawn()
+	local char = client:getChar()
+	char:setName("SCP-682, 'The Hard-To-Destroy Reptile'")
+	char:setDesc(self.desc)
+	lia.chat.send(client, "mind", "Your hatred for all life is overwhelming. You must destroy all life forms.")
+end
+
+CLASS_SCP_682 = CLASS.index
