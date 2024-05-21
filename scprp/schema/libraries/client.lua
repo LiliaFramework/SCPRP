edeb78743676b0cf247bf9a8baae756db408f36c
B0zy <40073828+B0zy@users.noreply.github.com>
2024-05-21 13:48:49 -0400

    Update client.lua

--- scprp/schema/libraries/client.lua
+++ scprp/schema/libraries/client.lua
@@ -1,16 +1,15 @@
-local color_682 = {}
-
-color_682[ "$pp_colour_addr" ] = 0,
-color_682[ "$pp_colour_addg" ] = 0,
-color_682[ "$pp_colour_addb" ] = 0,
-color_682[ "$pp_colour_brightness" ] = -0.7 or -0.3,
-color_682[ "$pp_colour_contrast" ] = 1,
-color_682[ "$pp_colour_colour" ] = 1,
-color_682[ "$pp_colour_mulr" ] = 5,
-color_682[ "$pp_colour_mulg" ] = 0,
-color_682[ "$pp_colour_mulb" ] = 0
 function SCHEMA:RenderScreenspaceEffects()
 	if LocalPlayer():getClass() == CLASS_SCP_682 then
-		DrawColorModify(color_682)
+		DrawColorModify({
+			[ "$pp_colour_addr" ] = 0,
+			[ "$pp_colour_addg" ] = 0,
+			[ "$pp_colour_addb" ] = 0,
+			[ "$pp_colour_brightness" ] = -0.7 or -0.3,
+			[ "$pp_colour_contrast" ] = 1,
+			[ "$pp_colour_colour" ] = 1,
+			[ "$pp_colour_mulr" ] = 5,
+			[ "$pp_colour_mulg" ] = 0,
+			[ "$pp_colour_mulb" ] = 0
+		})
 	end
 end
