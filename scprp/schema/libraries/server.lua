63cf9542599d18e9dad2ffdf7a7cb87021a97789
Bozy <40073828+B0zy@users.noreply.github.com>
2024-05-25 01:51:02 -0400

    Update server.lua

--- scprp/schema/libraries/server.lua
+++ scprp/schema/libraries/server.lua
@@ -2,7 +2,7 @@ function SCHEMA:PlayerSwitchFlashlight(_)
 	return true
 end
 
-function lia.scp.getSCPClasses()
+function SCHEMA:getSCPClasses()
 	local classes = {}
 	for _, v in pairs(lia.class.classes) do
 		if v.faction == FACTION_SCP then
