28d7b82c89aa6fe757b52e221b92b57f25cdba90
Bozy <40073828+B0zy@users.noreply.github.com>
2024-07-17 19:07:46 -0400

    Update classd.lua

--- scprp/schema/factions/classd.lua
+++ scprp/schema/factions/classd.lua
@@ -19,7 +19,7 @@ FACTION.isDefault = true
 FACTION.models = {"models/player/cheddar/class_d/class_d_art.mdl", "models/player/cheddar/class_d/class_d_erdim.mdl", "models/player/cheddar/class_d/class_d_eric.mdl", "models/player/cheddar/class_d/class_d_joe.mdl", "models/player/cheddar/class_d/class_d_mike.mdl", "models/player/cheddar/class_d/class_d_sandro.mdl", "models/player/cheddar/class_d/class_d_ted.mdl", "models/player/cheddar/class_d/class_d_van.mdl", "models/player/cheddar/class_d/class_d_vance.mdl"}
 function FACTION:OnCharCreated(_, character)
     local name = character:getName()
-    character:setName("D-" .. math.radom(1111, 9999) .. " " .. name)
+    character:setName("D-" .. math.random(1111, 9999) .. " " .. name)
 end
 
 FACTION_CLASSD = FACTION.index
