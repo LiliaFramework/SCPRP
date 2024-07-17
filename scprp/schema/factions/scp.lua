fe108645211a8171613c4ff1ed6ae0d0e8de3e15
Bozy <40073828+B0zy@users.noreply.github.com>
2024-07-16 23:57:28 -0400

    Update scp.lua

--- scprp/schema/factions/scp.lua
+++ scprp/schema/factions/scp.lua
@@ -16,7 +16,7 @@ FACTION.desc = "Select your SCP."
 FACTION.color = Color(68, 68, 68)
 FACTION.isDefault = true
 function FACTION:GetDefaultName(_)
-    return "SCP" .. math.radom(11111, 99999)
+    return "SCP" .. math.random(11111, 99999)
 end
 
 function FACTION:GetDefaultDesc(_, _)
