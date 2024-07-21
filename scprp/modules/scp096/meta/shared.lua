bbd7a213a779eede9ea9260ee478eba7ea71e62f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-21 16:25:39 +0100

    Update shared.lua

--- scprp/modules/scp096/meta/shared.lua
+++ scprp/modules/scp096/meta/shared.lua
@@ -1,13 +1,19 @@
-﻿--- Meta Tables for SCP-096.
+--- Meta Tables for SCP-096.
 -- @group Player
 -- @modmeta SCP096
 
 local playerMeta = FindMetaTable("Player")
 
+--- Checks if the player has been spotted by SCP-096.
+-- @return bool `true` if spotted, `false` otherwise.
+-- @realm shared
 function playerMeta:HuntedBySCP096()
     return self:GetNWBool("spotted_096", false)
 end
 
+--- Checks if the player is SCP-096.
+-- @return bool `true` if the player is SCP-096, `false` otherwise.
+-- @realm shared
 function playerMeta:isSCP096()
     local weapon = self:GetActiveWeapon()
     if not IsValid(weapon) then return false end
@@ -15,15 +21,19 @@ function playerMeta:isSCP096()
 end
 
 if SERVER then
-    local WalkSpeed = lia.config.WalkSpeed
-    local RunSpeed = lia.config.RunSpeed
+    --- Applies a speed modification to the player.
+    -- @number amount The multiplier for the walk and run speed.
+    -- @bool bool `true` to apply the speed modification, `false` to reset to default.
+    -- @realm shared
     function playerMeta:ApplySpeed(amount, bool)
-        self:SetWalkSpeed((bool and WalkSpeed * tonumber(amount)) or WalkSpeed)
-        self:SetRunSpeed((bool and RunSpeed * tonumber(amount)) or RunSpeed)
+        self:SetWalkSpeed((bool and lia.config.WalkSpeed * tonumber(amount)) or lia.config.WalkSpeed)
+        self:SetRunSpeed((bool and lia.config.RunSpeed * tonumber(amount)) or lia.config.RunSpeed)
     end
 
+    --- Sets the player’s speed to a reduced value when SCP-096 is spawned.
+    -- @realm shared
     function playerMeta:SCP096ApplySpawnSpeed()
-        self:SetWalkSpeed(WalkSpeed * 0.5)
-        self:SetRunSpeed(RunSpeed * 0.5)
+        self:SetWalkSpeed(lia.config.WalkSpeed * 0.5)
+        self:SetRunSpeed(lia.config.RunSpeed * 0.5)
     end
 end
