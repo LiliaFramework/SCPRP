eac2f301614a1456f43ad90a9e4d255ac8d86ef6
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-26 06:22:07 +0000

    Cleanup

--- scprp/schema/schema.lua
+++ scprp/schema/schema.lua
@@ -1,30 +1,3 @@
-﻿--[[--
-    Schema configuration options.
-
-    This is meant to define the Schema's default information.
-]]
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-
-=======
--- @configuration Schema
---- A list of schema fields that define the gamemode you are making.
--- These options are defined by the 'SCHEMA' table.
--- @realm shared
--- @table Configurations
--- @field name Controls the name of the schema | **string**
--- @field author Specifies the author of the schema | **string**
--- @field desc Defines the description of the schema | **string**
--- @field SCPs A list of SCPs that are in the schema | **table**
->>>>>>> fa3e6fbb8815b716f99a2856212141398f3743e7
-SCHEMA.name = "SCPRP"
+﻿SCHEMA.name = "SCPRP"
 SCHEMA.author = "Sample Schema Author"
 SCHEMA.desc = "A SCPRP Schema"
